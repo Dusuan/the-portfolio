3b14bddb53eb3034d17f1977f9da0b73d563825e
Dusuan <XxDusuanxX@hotmail.com>
2025-02-10 21:17:28 -0800

    Letsgoooo, modal fixed

--- src/app/LayoutClient.tsx
+++ src/app/LayoutClient.tsx
@@ -1,15 +1,11 @@
 "use client";
-import { AppProvider } from "./AppContext";
+import { AppProvider, useAppContext } from "./AppContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PrincipalHeader from "./myComponents/PrincipalHeader";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import Me from "./myComponents/Me";
-import { Separator } from "@/components/ui/separator";
-import React, { useState } from "react";
-import { useAppContext } from "./AppContext";
-import { create } from "domain";
 import PdfModal from "./myComponents/pdfModal";
 
 const geistSans = Geist({
@@ -38,48 +34,38 @@ export type LayoutProps = {
   children: React.ReactNode;
 };
 
-export default function RootLayout({ children }: LayoutProps) {
-  const appContext = useAppContext();
-  if (!appContext) {
-    throw new Error("useAppContext must be used within an AppProvider");
-  }
-  const { isOpen, setIsOpen } = appContext;
-  return (
-    <AppProvider>
-      <html lang="en" className="scroll-smooth">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-neutral-900`}
-        >
-          <AppRouterCacheProvider>
-            <PdfModalWrapper>
-              <div className="min-h-svh md:mx-16 xl:mx-60 2xl:mx-72 bg-neutral-900 px-10">
-                <PrincipalHeader Buttons={Buttons} />
-                <div className="flex lg:hidden">
-                  <Me isOpen={isOpen} setIsOpen={setIsOpen} />
-                </div>
-                <div className="flex h-full justify-center items-start">
-                  <div className="hidden lg:flex sticky top-20 mx-10 2xl:mx-20 justify-center  text-white">
-                    <Me isOpen={isOpen} setIsOpen={setIsOpen} />
-                  </div>
-
-                  {children}
-                </div>
-              </div>
-            </PdfModalWrapper>
-          </AppRouterCacheProvider>
-        </body>
-      </html>
-    </AppProvider>
-  );
-}
-
-function PdfModalWrapper({ children }: { children: React.ReactNode }) {
+function LayoutContent({ children }: LayoutProps) {
   const { isOpen, setIsOpen } = useAppContext();
 
   return (
-    <>
-      <PdfModal isOpen={isOpen} setIsOpen={setIsOpen} />
-      {children}
-    </>
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-neutral-900`}
+      >
+        <AppRouterCacheProvider>
+          <div className="min-h-svh md:mx-16 xl:mx-60 2xl:mx-72 bg-neutral-900 px-10">
+            <PrincipalHeader Buttons={Buttons} />
+            <div className="flex lg:hidden">
+              <Me isOpen={isOpen} setIsOpen={setIsOpen} />
+            </div>
+            <div className="flex h-full justify-center items-start">
+              <div className="hidden lg:flex sticky top-20 mx-10 2xl:mx-20 justify-center text-white">
+                <Me isOpen={isOpen} setIsOpen={setIsOpen} />
+              </div>
+              {children}
+            </div>
+          </div>
+          <PdfModal isOpen={isOpen} setIsOpen={setIsOpen} />
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <AppProvider>
+      <LayoutContent>{children}</LayoutContent>
+    </AppProvider>
   );
 }
