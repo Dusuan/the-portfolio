a96f46f15365458a796c19f407eefa36c160fd94
Dusuan <XxDusuanxX@hotmail.com>
2025-02-14 10:07:49 -0800

    bettering the projects info

--- src/app/myComponents/ProjectCarousel.tsx
+++ src/app/myComponents/ProjectCarousel.tsx
@@ -17,19 +17,22 @@ import ProjectTiles from "./ProjectTiles";
 
 const projects = [
   {
-    title: "Project 1",
-    description: "This is a description for project 1",
-    image: "/dusuanpelon.jpg",
+    title: "Rock Fundadores Movile App",
+    description: "Inventory management app for a local business",
+    image: "/rf.png",
+    link: "https://github.com/Dusuan/RockFundadores-APP",
   },
   {
-    title: "Project 2",
-    description: "This is a description for project 2",
-    image: "/dusuan1.jpg",
+    title: "GitAutoProjects",
+    description: "API that scrapes GitHub repositories and returns a list of pinned projects",
+    image: "/gittthub.png",
+    link: "https://github.com/Dusuan/gitautoproyects",
   },
   {
-    title: "Project 3",
-    description: "This is a description for project 3",
-    image: "/dusuanA.jpg",
+    title: "SDG's Challenge",
+    description: "App developed to help kids learn about the SDG's, developed for the SpaceApps Hackathon", 
+    image: "/sdgs.png",
+    link: "https://github.com/Dusuan/SpaceAppsHackathon",
   },
 ];
 
@@ -37,7 +40,9 @@ export default function ProjectCarousel() {
   return (
     <div className="flex flex-col justify-center">
       <div className="mt-2 text-center lg:text-start">
-        <p className="m-0 text-5xl md:text-6xl xl:text-7xl font-extrabold">RECENT </p>
+        <p className="m-0 text-5xl md:text-6xl xl:text-7xl font-extrabold">
+          RECENT{" "}
+        </p>
         <p className="m-0 text-5xl md:text-6xl xl:text-7xl font-extrabold mb-6 text-neutral-600">
           PROJECTS
         </p>{" "}
@@ -73,7 +78,13 @@ export default function ProjectCarousel() {
       </div>
       <div className="hidden lg:flex lg:flex-col justify-center">
         {projects.map((project, index) => (
-          <ProjectTiles key={index} description={project.description} title={project.title} image={project.image} />
+          <ProjectTiles
+            key={index}
+            description={project.description}
+            title={project.title}
+            image={project.image}
+            link={project.link}
+          />
         ))}
       </div>
     </div>
--- src/app/myComponents/ProjectTiles.tsx
+++ src/app/myComponents/ProjectTiles.tsx
@@ -4,46 +4,50 @@ type ProjectTilesProps = {
   title: string;
   description: string;
   image: string;
+  link: string;
 };
 
 export default function ProjectTiles({
   title,
   image,
   description,
+  link,
 }: ProjectTilesProps) {
   return (
-    <div className="flex py-2 w-full h-44">
-      <div className="w-full bg-neutral-900 group hover:bg-neutral-800 duration-200 ease-out rounded-lg p-4">
-        <div className="flex justify-between text-white">
-          <div className="flex items-center">
-            <div>
-              <img
-                className="rounded-lg max-h-[125px] max-w-[125px] "
-                src={image}
-                width={125}
-                height={125}
-                alt="project"
-              />
+    <a href={link} target="_blank" rel="noreferrer">
+      <div className="flex py-2 w-full h-44">
+        <div className="w-full bg-neutral-900 group hover:bg-neutral-800 duration-200 ease-out rounded-lg p-4">
+          <div className="flex justify-between text-white">
+            <div className="flex items-center">
+              <div>
+                <img
+                  className="rounded-lg max-h-[125px] max-w-[125px] "
+                  src={image}
+                  width={125}
+                  height={125}
+                  alt="project"
+                />
+              </div>
+              <div className="mx-8">
+                <h1 className="text-2xl font-bold">{title}</h1>
+                <p className="font-mono text-inherit text-neutral-400">
+                  {description}
+                </p>
+              </div>
             </div>
-            <div className="mx-8">
-              <h1 className="text-2xl font-bold">{title}</h1>
-              <p className="font-mono text-inherit text-neutral-400">
-                {description}
-              </p>
-            </div>
-          </div>
 
             <div className="group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform duration-200 ">
               <Image
-              src="/diagonal-arrow.svg"
-              alt="arrow"
-              width={30}
-              height={30}
-              className="w-8 h-8"
+                src="/diagonal-arrow.svg"
+                alt="arrow"
+                width={30}
+                height={30}
+                className="w-8 h-8"
               />
             </div>
+          </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 }
